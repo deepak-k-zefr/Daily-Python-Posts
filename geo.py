7c263ef66ff9cd4126460f3be400442c3f210e61
dualphase90 <dvkaruna@asu.edu>
2017-10-18 01:33:08 -0500

    returns physical address from lat long

---
+++ geo.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 19 14:39:22 2017
+
+@author: dualpahse_ai
+"""
+
+import requests
+
+
+def example():
+    # grab some lat/long coords from wherever. For this example,
+    # I just opened a javascript console in the browser and ran:
+    #
+    # navigator.geolocation.getCurrentPosition(function(p) {
+    #   console.log(p);
+    # })
+    #
+    latitude = 35.1330343
+    longitude = -90.0625056
+
+    # Did the geocoding request comes from a device with a
+    # location sensor? Must be either true or false.
+    sensor = 'true'
+
+    # Hit Google's reverse geocoder directly
+    # NOTE: I *think* their terms state that you're supposed to
+    # use google maps if you use their api for anything.
+    base = "http://maps.googleapis.com/maps/api/geocode/json?"
+    params = "latlng={lat},{lon}&sensor={sen}".format(
+        lat=latitude,
+        lon=longitude,
+        sen=sensor
+    )
+    url = "{base}{params}".format(base=base, params=params)
+    response = requests.get(url)
+    return response.json()['results'][0]['formatted_address']
+
+print(example())
